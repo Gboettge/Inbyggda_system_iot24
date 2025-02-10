04939b93824f40020344177e887ae0c2488a7d22
Gboettge <gustav.boettge@outlook.com>
2025-02-10 09:10:33 +0100

    +

--- Ovning/Ovning_5_I2C/main/main.c
+++ Ovning/Ovning_5_I2C/main/main.c
@@ -2,6 +2,7 @@
 #include "driver/i2c_master.h"
 #include "driver/i2c_slave.h"
 #include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
 
 // i2c_master_bus_config_t i2c_mst_config = {
 //     .clk_source = I2C_CLK_SRC_DEFAULT,
@@ -33,11 +34,20 @@ i2c_slave_config_t i2c_slv_config = {
 };
 
 i2c_slave_dev_handle_t slave_handle;
+typedef enum {
+    I2C_SLAVE_EVT_RX,
+    I2C_SLAVE_EVT_TX
+} i2c_slave_event_t;
+
 
 void app_main(void)
 {
-// ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
-// ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
-ESP_ERROR_CHECK(i2c_new_slave_device(&i2c_slv_config, &slave_handle));
-
+    // ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+    // ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+        ESP_ERROR_CHECK(i2c_new_slave_device(&i2c_slv_config, &slave_handle));
+    while (1)
+    {
+        printf("Hej\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
