f981b097d71169943738f9dd3b6f5ef235ef5201
Gboettge <gustav.boettge@outlook.com>
2025-02-12 22:30:19 +0100

    Button klar

--- Uppgift/Uppgift_1_komponent/components/Button/Button.c
+++ Uppgift/Uppgift_1_komponent/components/Button/Button.c
@@ -1,6 +1,7 @@
 #include "Button.h"
 
 
+
 button_handle button_init(int pin, strapping_mode mode){
 
     button_handle new_button = malloc (sizeof(button));
@@ -11,6 +12,12 @@ button_handle button_init(int pin, strapping_mode mode){
     new_button->onpressed = NULL;
     gpio_input_enable(pin);
     gpio_intr_disable(pin);
+
+    new_button->current_state = STATE_OFF;
+    new_button->next_state = STATE_OFF;
+    new_button->previous_state = STATE_OFF;
+    new_button->level = 0;
+    new_button->debounce_tick = 0;
     
     if(mode == GPIO_PULLUP){
         gpio_set_pull_mode(pin, GPIO_PULLUP_ONLY);
@@ -23,43 +30,65 @@ button_handle button_init(int pin, strapping_mode mode){
 
 //debounce (latch?)
 // 10ms max per uppdatering
-void button_update(button_handle button){
-    TickType_t startTick = xTaskGetTickCount();
-
-    if (button->mode == GPIO_PULLDOWN)    
-        button->pressed = gpio_get_level(button->pin); // tar inte bort debounce
-    if (button->mode == GPIO_PULLUP){
-        button->pressed = !gpio_get_level(button->pin);
-        // button->pressed = false;
-        // button->latch = 1;
-    }    
-
-    if (button->pressed && !button->latch){
-         TickType_t tickDifference = xTaskGetTickCount() - startTick;
-            button->latch = 1;
-        //if (tickDifference >= pdMS_TO_TICKS(1))
-        button->pressed = true;
-        if(button->onpressed != NULL)
-            button->onpressed(button->pin);
-        else
-            printf("pressed %d\n", button->pressed);
-        return;
-    }
-    else if (!button->pressed && button->latch){
-         TickType_t tickDifference = xTaskGetTickCount() - startTick;
-            button->latch = 0;
-        //if (tickDifference >= pdMS_TO_TICKS(1))
-        button->pressed = false;
-        //printf("not pressed %d\n", button->pressed);
-        return;
-    }
-    //ingen vtask delay här
-   vTaskDelay(1);
-   //printf("ticks passed: %ld\n", startTick/*xTaskGetTickCount()*/);
+void button_update(button_handle button)
+{
+    TickType_t current_tick = xTaskGetTickCount();
+    if (button->mode == GPIO_PULLDOWN)
+        button->level = gpio_get_level(button->pin); // tar inte bort debounce
+    if (button->mode == GPIO_PULLUP)
+        button->level = !gpio_get_level(button->pin);
     
-}
+    switch (button->current_state)
+    {
 
-//ordna ifall man nyttjar pullup istäället för pullup
+    case STATE_ON:
+        button->pressed = 1;
+        if (button->level == 0)
+        {
+            button->next_state = DEBOUNCE_STATE_ON;
+            button->debounce_tick = current_tick;
+        }
+        break;
+    case DEBOUNCE_STATE_ON:
+        button->pressed = 0;
+        if (current_tick - button->debounce_tick >= pdMS_TO_TICKS(30))
+        {
+            button->next_state = STATE_OFF;
+            if (button->onpressed != NULL)
+            {
+                button->onpressed(button->pin);
+            }
+            else {
+                printf("pressed %d\n", button->pressed);
+            }
+        }
+        break;
+    case STATE_OFF:
+        button->pressed = 0;
+        if (button->level == 1)
+        {
+            button->next_state = DEBOUNCE_STATE_OFF;
+            button->debounce_tick = current_tick;
+        }
+        break;
+    case DEBOUNCE_STATE_OFF:
+        button->pressed = 1;
+        if (current_tick - button->debounce_tick >= pdMS_TO_TICKS(30))
+        {
+            button->next_state = STATE_ON;
+        }
+        break;
+    default:
+        printf("error %d\n", button->current_state);
+        break;
+    }
+    if(button->next_state != button->current_state){
+        printf("%d -> %d\n", button->current_state, button->next_state);
+    }
+    button->previous_state = button->current_state;
+    button->current_state = button->next_state;
+}
+   
 bool button_isPressed (button_handle button){
     return button->pressed;
 }
@@ -90,3 +119,53 @@ lista
 index
 adc värde
 */
+
+
+/*
+void button_update(button_handle button)
+{
+    TickType_t startTick = xTaskGetTickCount();
+
+    if (button->mode == GPIO_PULLDOWN)
+        button->pressed = gpio_get_level(button->pin); // tar inte bort debounce
+    if (button->mode == GPIO_PULLUP)
+    {
+        button->pressed = !gpio_get_level(button->pin);
+        // button->pressed = false;
+        // button->latch = 1;
+    }
+
+    if (button->pressed && !button->latch)
+    {
+        TickType_t tickDifference = xTaskGetTickCount() - startTick;
+        if (tickDifference >= pdMS_TO_TICKS(200))
+        {
+            button->latch = 1;
+            button->pressed = true;
+        }
+
+        if (button->onpressed != NULL)
+            button->onpressed(button->pin);
+        else
+            printf("pressed %d\n", button->pressed);
+        return;
+    }
+    else if (!button->pressed && button->latch)
+    {
+        TickType_t tickDifference = xTaskGetTickCount() - startTick;
+
+        if (tickDifference >= pdMS_TO_TICKS(200))
+        {
+        }
+        button->latch = 0;
+        button->pressed = false;
+        //  printf("ticks passed: %ld\n", tickDifference);
+        // printf("not pressed %d\n", button->pressed);
+        return;
+    }
+    // ingen vtask delay här
+    vTaskDelay(1);
+    // printf("ticks passed: %ld\n", startTick);
+    */
+
+//ordna ifall man nyttjar pullup istäället för pullup
--- Uppgift/Uppgift_1_komponent/components/Button/include/Button.h
+++ Uppgift/Uppgift_1_komponent/components/Button/include/Button.h
@@ -8,12 +8,25 @@ typedef enum {
     GPIO_PULLDOWN 
 }strapping_mode;
 
+typedef enum{
+    STATE_ON,
+    DEBOUNCE_STATE_ON,
+    STATE_OFF,
+    DEBOUNCE_STATE_OFF
+}state_e;
+
 typedef struct {
     int pin;
+    //state_e button_state;
     strapping_mode mode;
     int latch;
     bool pressed;
     void (*onpressed)(int pin);
+    state_e current_state;
+    state_e next_state;
+    state_e previous_state;
+    int level;
+    TickType_t debounce_tick;
 } button;
 
 typedef button* button_handle;
