21977aabd2cfd8f95805525fa1591d0f934e3475
Gboettge <gustav.boettge@outlook.com>
2025-02-13 17:49:31 +0100

    +

--- Uppgift/Uppgift_1_komponent/components/Potentiometer/Potentiometer.c
+++ Uppgift/Uppgift_1_komponent/components/Potentiometer/Potentiometer.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 #include "Potentiometer.h"
 
-pot_handle potentiometer_init(int pin);
+
+pot_handle potentiometer_init(GPIO_ADC pin, ADC_LIMIT limit){
+    pot_handle newPot = pvPortMalloc(sizeof(pot_t));
+    newPot->adc_raw = 0;
+    newPot->channel_config = channel;
+    newPot->limit = limit;
+    newPot->pin = pin;
+    newPot->config.atten = limit;
+    newPot->config.bitwidth = ADC_BITWIDTH_DEFAULT;
+}
+
+
+xxx init ( pin/adc, xxx )
+
+xxx update (xxx)
+uppdaterar = 
+
+int getValue( xxx )
+
+xxx setOnThreshold ( int threshold, bool risingEdge, xxx (*onThreshold)(int pin/adc, value, xxx), xxx )
+
