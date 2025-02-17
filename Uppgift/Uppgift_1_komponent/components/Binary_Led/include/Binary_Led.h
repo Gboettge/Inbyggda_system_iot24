d8d777783de45ad2b5b49bc8c81a02cf9071ef3a
Gboettge <gustav.boettge@outlook.com>
2025-02-17 12:19:51 +0100

    +

--- Uppgift/Uppgift_1_komponent/components/Binary_Led/include/Binary_Led.h
+++ Uppgift/Uppgift_1_komponent/components/Binary_Led/include/Binary_Led.h
@@ -1,1 +1,31 @@
-void func(void);
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+
+typedef enum{
+    STATE_ON,
+    STATE_OFF,
+    BLINK_ON,
+    BLINK_OFF
+}b_led_state_e;
+
+typedef struct{
+    gpio_config_t gpio_config;
+    gpio_num_t pin;
+    int level;
+    int milisecOn;
+    int milisecOff;
+    b_led_state_e current_state;
+    b_led_state_e next_state;
+    
+}Bin_LED;
+
+typedef Bin_LED* Bin_LED_handle;
+
+Bin_LED_handle init(int pin);
+
+void update(Bin_LED_handle LED);
+
+void setLed(Bin_LED_handle LED, int value);
+
+void blink(Bin_LED_handle LED, int milisecOn, int milisecOff);
+
