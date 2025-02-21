bfbe815d2dca6d03a4d4c9c93abe5b3179484202
Gboettge <gustav.boettge@outlook.com>
2025-02-21 17:32:17 +0100

    RGB mall klar fixa komponent

--- Uppgift/Uppgift_1_komponent/main/main.c
+++ Uppgift/Uppgift_1_komponent/main/main.c
@@ -8,38 +8,51 @@ button_handle btn1;
 button_handle btn2;
 button_handle btn3;
 pot_handle adc;
-b_led_handle b_Led;
-a_led_handle a_Led;
-a_led_handle a_Led1;
+// b_led_handle b_Led;
+a_led_handle red;
+a_led_handle green;
+a_led_handle blue;
+int value = 0;
+int green_value = 0;
+int blue_value = 0;
 
+void changeRed();
+void changeGreen();
+void changeBlue();
 
 void app_main(void)
 {
+
     printf("Starting main\n");
     adc = potentiometer_init(ADC_UNIT_1, ADC_CHANNEL_1);
-    btn1 = button_init(GPIO_NUM_2, GPIO_PULLDOWN);
-    btn2 = button_init(GPIO_NUM_3, GPIO_PULLDOWN);
-    btn3 = button_init(GPIO_NUM_23, GPIO_PULLUP);
+    btn1 = button_init(GPIO_NUM_18, GPIO_PULLDOWN);
+    btn2 = button_init(GPIO_NUM_20, GPIO_PULLDOWN);
+    btn3 = button_init(GPIO_NUM_21, GPIO_PULLDOWN);
     
-    b_Led = b_led_init(GPIO_NUM_9, LIGHT_OFF, false);
-    a_Led = a_led_init(18, 0, 0);
-    a_Led1 = a_led_init(21, 1, 1);
+    // b_Led = b_led_init(GPIO_NUM_9, LIGHT_OFF, false);
+    red = a_led_init(2,0,0);
+    green = a_led_init(3,1,0);
+    blue = a_led_init(4,2,0);
     
-    button_setOnPressed(btn1, button_print_press_num);
-    button_setOnPressed(btn2, button_print_num_x_ten);
-    button_setOnReleased(btn1, button_print_released);
-    button_setOnReleased(btn2, button_print_released);
+    button_setOnPressed(btn1, changeRed);
+    button_setOnPressed(btn2, changeGreen);
+    button_setOnPressed(btn3, changeBlue);
+    // button_setOnPressed(btn2, button_print_num_x_ten);
+    // button_setOnReleased(btn1, button_print_released);
+    // button_setOnReleased(btn2, button_print_released);
     pot_setOnThreshold(adc, 3000, pot_printThresholdReach);
     
-    b_led_setled(b_Led, LIGHT_ON);
+    // b_led_setled(b_Led, LIGHT_ON);
     //b_led_blink(b_Led, 50, 550);
 
     
-    a_led_setLed(a_Led, 4000);
-    a_led_setLed(a_Led1, 300);
+    a_led_setLed(blue, 0);
+    a_led_setLed(red, 0);
+    a_led_setLed(green, 0);
 
-    a_led_sin(a_Led, 100);
-    a_led_sin(a_Led1, 300);
+    a_led_sin(red, 300); //
+    a_led_sin(green, 400); //
+    //a_led_sin(blue, 300);
     
     
     int adc_value;//
@@ -50,23 +63,58 @@ void app_main(void)
         button_update(btn1);
         button_update(btn2);
         button_update(btn3);
-        b_led_update(b_Led);
-        a_led_update(a_Led);
-        a_led_update(a_Led1);
-        adc_value = pot_get_value(adc);//
-        if (adc_value <80){
-            adc_value = 0;
+        // b_led_update(b_Led);
+        a_led_update(red);
+        a_led_update(green);
+        a_led_update(blue);
 
-        }
         
-        a_led_setLed(a_Led1, adc_value); //
+        // adc_value = pot_get_value(adc);//
+        // if (adc_value <80){
+        //     adc_value = 0;
+        //     printf("value:%d\n", adc_value);
+        // }
+        // a_led_setLed(blue, adc_value); //
         
     }
     // button_destroy(&btn1);
     // button_destroy(&btn2);
     // button_destroy(&btn3);
     // pot_destroy(&adc);
-    b_led_destroy(&b_Led);
-    a_led_destroy(&a_Led);
-    a_led_destroy(&a_Led1);
+    // b_led_destroy(&b_Led);
+    a_led_destroy(&red);
+    
+}
+
+
+void changeRed(){
+    value += 400;
+    if (value > 4050){
+        
+        value = 0;
+        
+    }
+    printf("red val: %d\n", value);
+    a_led_setLed(red, value); //
+}
+
+void changeGreen(){
+    green_value += 400;
+    if (green_value > 4050){
+        
+        green_value = 0;
+        
+    }
+    printf("green val: %d\n", green_value);
+    a_led_setLed(green, green_value); //
+}
+void changeBlue(){
+    blue_value += 400;
+    if (blue_value > 4050){
+
+        blue_value = 0;
+
+    }
+    printf("blue val: %d\n", blue_value);
+    a_led_setLed(blue, blue_value); //
 }
