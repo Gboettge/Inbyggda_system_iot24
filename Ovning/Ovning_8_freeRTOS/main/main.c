bf7765687d0733922cc4eb5a6a47b73ce8fdf591
Gboettge <gustav.boettge@outlook.com>
2025-03-05 15:11:36 +0100

    +

--- Ovning/Ovning_8_freeRTOS/main/main.c
+++ Ovning/Ovning_8_freeRTOS/main/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+
 // #include "Button.h"
 #include "Potentiometer.h"
 #include "Binary_led.h"
 #include "Analog_Led.h"
+
+const char * TAG = "Main";
+
 void aledSetOn(void* param);
 
 a_led_handle aled;
@@ -16,10 +22,11 @@ void app_main(void)
     aled = a_led_init(4, 0, 0, 0);
     a_led_sin(aled, 200);
     
+    ESP_LOGI(TAG, "app_main STARTING");
 
     xTaskCreate(
         aledSetOn,      // Function name of the task
-        "Blink 1",   // Name of the task (e.g. for debugging)
+        "Task 1",   // Name of the task (e.g. for debugging)
         2048,        // Stack size (bytes)
         NULL,        // Parameter to pass
         1,           // Task priority
@@ -28,7 +35,12 @@ void app_main(void)
     
 }
 void aledSetOn(void* param){
-    while(1){
-        
+    ESP_LOGI(TAG, "Task 1 function running");
+    int counter = param
+    while (true)
+    {
+        ESP_LOGI(TAG, "counter: %d", *counter++);
     }
+    ESP_LOGI(TAG, "Task 1 function returning");
+    vTaskDelete(NULL);
 }
