90fa3d8d985714698230c7f4a65842223dd8f5d1
Gboettge <gustav.boettge@outlook.com>
2025-03-05 09:54:12 +0100

    LCD-test uppdaterad

---
+++ Ovning/LCD komponent/components/my_lcd/my_lcd.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "my_lcd.h"
+
+void func(void)
+{
+
+}
--- Ovning/SCREEN_TEST_1/main/lvgl_demo_ui.c
+++ Ovning/SCREEN_TEST_1/main/lvgl_demo_ui.c
@@ -18,13 +18,13 @@ void example_lvgl_demo_ui(lv_disp_t *disp)
     lv_obj_t *label = lv_label_create(scr);
     lv_obj_t *label1 = lv_label_create(scr);
     lv_obj_t *label2 = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL); /* Circular scroll */
+    //lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL); /* Circular scroll */
     lv_label_set_text(label, "Hej baby");
 
-    lv_label_set_long_mode(label1, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
+    //lv_label_set_long_mode(label1, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
     lv_label_set_text(label1, "Jag kommer inte hem till middag...");
    
-    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL); /* Circular scroll */
+    //lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL); /* Circular scroll */
     lv_label_set_text(label2, "Ok? <3");
     /* Size of the screen (if you use rotation 90 or 270, please set disp->driver->ver_res) */
     lv_obj_set_width(label, disp->driver->hor_res);
