f93c801861fe03e33e28b5843a3b32872466c50f
Gboettge <gustav.boettge@outlook.com>
2025-03-12 14:40:39 +0100

    dispay ++

--- Projekt/Screen/components/DISPLAY1/include/DISPLAY1.h
+++ Projekt/Screen/components/DISPLAY1/include/DISPLAY1.h
@@ -1,1 +1,43 @@
-void func(void);
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "esp_lvgl_port.h"
+#include "lvgl.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ssd1306.h"
+#include <string.h>
+
+
+static const char *TAG = "Display";
+
+#define I2C_BUS_PORT  0
+
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ    (400 * 1000)
+#define EXAMPLE_PIN_NUM_SDA           6
+#define EXAMPLE_PIN_NUM_SCL           7
+#define EXAMPLE_PIN_NUM_RST           -1
+#define EXAMPLE_I2C_HW_ADDR           0x3C
+
+#define EXAMPLE_LCD_H_RES              128
+#define EXAMPLE_LCD_V_RES              64
+
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+
+typedef struct{
+    lv_disp_t *disp;
+}display_s;
+
+typedef  display_s * display_handle;
+
+display_s *display_init();
+
+// void display_ui(display_s *disp);
+void display_ui(display_s *display, char* leftTop, char* leftMid, char* leftButtom, char* score, char* time); //leftTop, leftMid, leftButtom, rightTop, rightBottom
--- Projekt/Screen/main/main.c
+++ Projekt/Screen/main/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "DISPLAY1.h"
 
 void app_main(void)
 {
 
+    display_s *lol = display_init();
+    display_ui(lol, "hej", NULL, "då", "", "");
 }
