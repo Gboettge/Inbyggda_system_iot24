4ad4ab40de878db0fb9f8ce67174a80d2119b76a
Gboettge <gustav.boettge@outlook.com>
2025-03-18 10:19:13 +0100

    +

--- Projekt/Projekt_v1_spel/components/Components/Helper_Component/include/Helper_Component.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace inbyggda_iot24
-{
-    int func(void);
-
-}
--- Projekt/Projekt_v1_spel/main/main.c
+++ Projekt/Projekt_v1_spel/main/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+#include "gameMaster.h"
+#include "Button.h"
+#include "Binary_Led.h"
+#include "DISPLAY1.h"
+#include "RGB_led_component.h"
 void app_main(void)
 {
 
